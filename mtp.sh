2e9f9a6fda27d73bd5a6be499e98e5b76364917f
antuone <asikuo@gmail.com>
2017-02-13 23:41:08 +0600

    Update mtp.sh

--- mtp.sh
+++ mtp.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+ftp_user="ekb"
+ftp_password="cb4yb5wf"
+ftp_ip="127.0.0.1"
+mkdir /mnt/ftp
+curlftpfs ${ftp_user}:${ftp_password}@${ftp_ip}/ /mnt/ftp -o allow_other
 su anton -c "export DISPLAY=:0; bash -c '/etc/udev/rules.d/sp/mtp1.sh $1 $2'"
-
+umount /mnt/ftp
+rmdir /mnt/ftp
